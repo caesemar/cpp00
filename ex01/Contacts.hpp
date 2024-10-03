03ca293df018a36f3b03da56d710cc546f620dce
Jose javier Casado martinez <jocasado@c1r12s6.42madrid.com>
2024-10-03 20:15:21 +0200

    encapsulation for contact private attributes

--- ex01/Contacts.hpp
+++ ex01/Contacts.hpp
@@ -6,7 +6,7 @@
 /*   By: jocasado <jocasado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 19:13:51 by jocasado          #+#    #+#             */
-/*   Updated: 2024/10/03 15:13:40 by jocasado         ###   ########.fr       */
+/*   Updated: 2024/10/03 19:56:08 by jocasado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,13 @@
     class contacts
     {
         public:
+            void setter(std::string first_name, std::string last_name, std::string nickname, std::string phone_number, std::string darkest_secret);
+            std::string get_first_name();
+            std::string get_last_name();
+            std::string get_nickname();
+            std::string get_phone_number();
+            std::string get_darkest_secret();
+        private:
             std::string _first_name;
             std::string _last_name;
             std::string _nickname;
--- ex01/ex01.cpp
+++ ex01/ex01.cpp
@@ -6,39 +6,81 @@
 /*   By: jocasado <jocasado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 17:27:06 by jocasado          #+#    #+#             */
-/*   Updated: 2024/10/03 15:35:18 by jocasado         ###   ########.fr       */
+/*   Updated: 2024/10/03 20:13:13 by jocasado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 
 #include "Phonebook.hpp"
+#include "Contacts.hpp"
 
+
+void contacts::setter(std::string first_name, std::string last_name, std::string nickname, std::string phone_number, std::string darkest_secret)
+{
+    _first_name = first_name;
+    _last_name = last_name;
+    _nickname = nickname;
+    _phone_number = phone_number;
+    _darkest_secret = darkest_secret;
+}
+
+std::string contacts::get_first_name()
+{
+    return (_first_name);
+}
+
+std::string contacts::get_last_name()
+{
+    return (_last_name);
+}
+
+std::string contacts::get_nickname()
+{
+    return (_nickname);
+}
+
+std::string contacts::get_phone_number()
+{
+    return (_phone_number);
+}
+
+std::string contacts::get_darkest_secret()
+{
+    return (_darkest_secret);
+}
 
 int Phonebook::Add(int _index)
 {
+    std::string _first_name;
+    std::string _last_name;
+    std::string _nickname;
+    std::string _phone_number;
+    std::string _darkest_secret;
+
     if (_index < 8)
     {
         std::cout << "Enter the first name: ";
-        std::getline(std::cin, _contacts[_index]._first_name);
+        std::getline(std::cin, _first_name);
         std::cout << "Enter the last name: ";
-        std::getline(std::cin, _contacts[_index]._last_name);
+        std::getline(std::cin, _last_name);
         std::cout << "Enter the nickname: ";
-        std::getline(std::cin, _contacts[_index]._nickname);
+        std::getline(std::cin,  _nickname);
         std::cout << "Enter the phone number: ";
-        std::getline(std::cin, _contacts[_index]._phone_number);
+        std::getline(std::cin, _phone_number);
         std::cout << "Enter the darkest secret: ";
-        std::getline(std::cin, _contacts[_index]._darkest_secret);
-        if (_contacts[_index]._first_name.empty() || _contacts[_index]._last_name.empty() || _contacts[_index]._nickname.empty() || _contacts[_index]._phone_number.empty() || _contacts[_index]._darkest_secret.empty())
+        std::getline(std::cin, _darkest_secret);
+        if (_first_name.empty() || _last_name.empty() || _nickname.empty() || _phone_number.empty() || _darkest_secret.empty())
         {
             std::cout << "Invalid contact" << std::endl;
-            _contacts[_index]._first_name.clear();
-            _contacts[_index]._last_name.clear();
-            _contacts[_index]._nickname.clear();
-            _contacts[_index]._phone_number.clear();
-            _contacts[_index]._darkest_secret.clear();
+            _first_name.clear();
+            _last_name.clear();
+            _nickname.clear();
+            _phone_number.clear();
+            _darkest_secret.clear();
             return 1;
         }
+        _contacts[_index].setter(_first_name, _last_name, _nickname, _phone_number, _darkest_secret);
     }
     return 0;
 }
@@ -48,9 +90,15 @@ void Phonebook::Search(int _index)
     int space_numer_1 = 0;
     int space_numer_2 = 0;
     int space_numer_3 = 0;
+    int i = 0;
     std::string first_name_short;
     std::string last_name_short;
     std::string nickname_short;
+    std::string first_name;
+    std::string last_name;
+    std::string nickname;
+    std::string phone_number;
+    std::string darkest_secret;
 
     if (_index == 0)
     {
@@ -59,23 +107,28 @@ void Phonebook::Search(int _index)
     else
     {
         std::cout << "     index|first name| last name|  nickname" << std::endl;
-        for (int i = 0; i < _index; i++)
+        while (i < _index)
         {
-            if (_contacts[i]._first_name.length() > 10)
-                first_name_short = _contacts[i]._first_name.substr(0, 9) + ".";
+            first_name = _contacts[i].get_first_name();
+            last_name = _contacts[i].get_last_name();
+            nickname = _contacts[i].get_nickname();
+            phone_number = _contacts[i].get_phone_number();
+            darkest_secret = _contacts[i].get_darkest_secret();
+            if (first_name.length() > 10)
+                first_name_short = first_name.substr(0, 9) + ".";
             else
-                first_name_short = _contacts[i]._first_name;
-            if (_contacts[i]._last_name.length() > 10)
-                last_name_short = _contacts[i]._last_name.substr(0, 9) + ".";
+                first_name_short = first_name;
+            if (last_name.length() > 10)
+                last_name_short = last_name.substr(0, 9) + ".";
             else
-                last_name_short = _contacts[i]._last_name;
-            if (_contacts[i]._nickname.length() > 10)
-                nickname_short = _contacts[i]._nickname.substr(0, 9) + ".";
+                last_name_short = last_name;
+            if (nickname.length() > 10)
+                nickname_short = nickname.substr(0, 9) + ".";
             else
-                nickname_short = _contacts[i]._nickname;
-            space_numer_1 = 10 - _contacts[i]._first_name.length();
-            space_numer_2 = 10 - _contacts[i]._last_name.length();
-            space_numer_3 = 10 - _contacts[i]._nickname.length();
+                nickname_short = nickname;
+            space_numer_1 = 10 - first_name.length();
+            space_numer_2 = 10 - last_name.length();
+            space_numer_3 = 10 - nickname.length();
             std::cout << "         " << i << "|";
             for (int j = 0; j < space_numer_1; j++)
                 std::cout << " ";
@@ -86,18 +139,19 @@ void Phonebook::Search(int _index)
             for (int j = 0; j < space_numer_3; j++)
                 std::cout << " ";
             std::cout << nickname_short << std::endl;
+            i++;
         }
         std::string index;
         std::cout << "Enter the index of the contact: ";
         std::getline(std::cin, index);
-        if (index.length() == 1 && index[0] >= '0' && index[0] <= '7')
+        if (index.length() == 1 && index[0] >= '0' && index[0] <= '7' && index[0] < i + '0')
         {
-            int i = index[0] - '0';
-            std::cout << "First name: " << _contacts[i]._first_name << std::endl;
-            std::cout << "Last name: " << _contacts[i]._last_name << std::endl;
-            std::cout << "Nickname: " << _contacts[i]._nickname << std::endl;
-            std::cout << "Phone number: " << _contacts[i]._phone_number << std::endl;
-            std::cout << "Darkest secret: " << _contacts[i]._darkest_secret << std::endl;
+            i = index[0] - '0';
+            std::cout << "First name: " << _contacts[i].get_first_name() << std::endl;
+            std::cout << "Last name: " << _contacts[i].get_last_name() << std::endl;
+            std::cout << "Nickname: " << _contacts[i].get_nickname()<< std::endl;
+            std::cout << "Phone number: " << _contacts[i].get_phone_number() << std::endl;
+            std::cout << "Darkest secret: " << _contacts[i].get_darkest_secret() << std::endl;
         }
         else
         {
@@ -123,11 +177,6 @@ int main()
     {
         std::cout << "Enter a command: ";
         std::getline(std::cin, command);
-        if (std::cin.eof())
-        {
-            phonebook.Exit();
-            break;
-        }
         if (command == "ADD")
         {
             if (index == 7)
